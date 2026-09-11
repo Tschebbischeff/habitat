3c01c07565984316c75c9b29991e0773af4d47da
Lontra Felina <26169724+Tschebbischeff@users.noreply.github.com>
2026-09-11 14:27:10 +0200

    sets healtchcheck to return healthy as soon as updates and upgrades begin

--- habitat-deploy/healthcheck.sh
+++ habitat-deploy/healthcheck.sh
@@ -3,7 +3,11 @@
 # shellcheck disable=SC2009  # pgrep does not show arguments
 STATUS_FILE="$(ps -o pid,args | grep -Po '^ *1 +.*/deploy.sh +\K[^ ]*')"
 
-[ -f "$STATUS_FILE" ] && [ "$(cat "$STATUS_FILE")" = "started" ] && \
-    exit 0
+[ -f "$STATUS_FILE" ] || exit 1
+currentStatus="$(cat "$STATUS_FILE")"
+[ "$currentStatus" = "update" ] && exit 0
+[ "$currentStatus" = "upgrade" ] && exit 0
+[ "$currentStatus" = "starting" ] && exit 0
+[ "$currentStatus" = "started" ] && exit 0
 
-exit 1
+exit 2
